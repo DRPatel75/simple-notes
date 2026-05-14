6c7dae9e172b74359854d672c341b191b3184c3e
DRPatel75 <drpatel3009@gmail.com>
2026-05-14 18:01:21 +0530

    added delete icon

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { MdDelete } from "react-icons/md";
 
 const Card = ({title, desc}) => {
   return (
     <div className='card'>
+      <div className="del">
+        <MdDelete />
+      </div>
         <div className="title">{title}</div>
         <div className="desc">{desc}</div>
     </div>
